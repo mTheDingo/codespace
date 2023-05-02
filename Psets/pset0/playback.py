97189bfeb85d3782f2b57701a7b3177ab96181c9
mTheDingo <114156590+mTheDingo@users.noreply.github.com>
2023-05-02 14:11:04 +0000

    Tue, May 2, 2023, 2:11 PM +00:00

---
+++ Psets/pset0/playback.py
@@ -0,0 +1,10 @@
+#implement a program in Python that prompts the user for input and then outputs that same input, replacing each space with ... (i.e., three periods).
+
+def main():
+    printUserInput()
+
+def printUserInput():
+    inp = input("What would you like to say ?")
+    print(inp, sep='...')
+
+main()
