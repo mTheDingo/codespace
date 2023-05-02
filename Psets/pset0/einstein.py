2422e4e8ea673745b8d9a25598183e6af520ec1c
mTheDingo <114156590+mTheDingo@users.noreply.github.com>
2023-05-02 15:59:45 +0000

    Tue, May 2, 2023, 3:59 PM +00:00

--- Psets/pset0/einstein.py
+++ Psets/pset0/einstein.py
@@ -4,11 +4,9 @@ and then outputs the equivalent number of Joules as an integer. Assume that the 
 
 
 # prompt the user for a mass as an integer
-mass = int(input("Mass: "))
+m = int(input("Mass: "))
 # output the equivalent number of joules as an integer
-E = 0.000239006
-m = mass
-c =300000000/1
+c =300000000
 E = pow(m + c, 2)
 
 print(E)
