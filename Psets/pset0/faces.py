5cb1659a536f5abfa1115cc52431ed1ac2bd2eca
mTheDingo <114156590+mTheDingo@users.noreply.github.com>
2023-05-02 15:19:06 +0000

    Tue, May 2, 2023, 3:19 PM +00:00

---
+++ Psets/pset0/faces.py
@@ -0,0 +1,17 @@
+"""
+implement a function called convert that accepts a str as input and returns that same input with any :) converted to 🙂
+(otherwise known as a slightly smiling face) and any :( converted to 🙁 (otherwise known as a slightly frowning face).
+All other text should be returned unchanged.
+"""
+
+def main():
+    convert()
+
+
+def convert():
+    string = input(" :   ").replace(" :) ", "🙂").replace( " :( ", "🙁")
+    print(string)
+
+
+
+    main()
