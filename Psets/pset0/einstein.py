0d5935f1cae729aff8e0b45c525bf97f0e216e64
mTheDingo <114156590+mTheDingo@users.noreply.github.com>
2023-05-02 15:45:21 +0000

    Tue, May 2, 2023, 3:45 PM +00:00

---
+++ Psets/pset0/einstein.py
@@ -0,0 +1,8 @@
+"""In a file called einstein.py, implement a program in Python that prompts the user for mass as an integer (in kilograms)
+and then outputs the equivalent number of Joules as an integer. Assume that the user will input an integer.
+"""
+
+
+# prompt the user for a mass as an integer
+# output the equivalent number of joules as an integer
+
