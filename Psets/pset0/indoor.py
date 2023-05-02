d84bf59c833375e261abf1c080f1c70a614ccefa
mTheDingo <114156590+mTheDingo@users.noreply.github.com>
2023-05-02 13:55:09 +0000

    Tue, May 2, 2023, 1:55 PM +00:00

--- Psets/pset0/indoor.py
+++ Psets/pset0/indoor.py
@@ -1,6 +1,15 @@
+"""
+implement a program in Python that prompts the user for input and then outputs that same input in lowercase.
+
+"""
+
 def main():
-    printuserinput()
+    printUserInput()
 
 
 def printUserInput():
-    input("What would you like to say?")
+   inp = input("What would you like to say?").lower()
+   print(inp)
+
+
+main()
