e38aea8cc45c9818a51cb1cbc5862f75120c4367
mTheDingo <114156590+mTheDingo@users.noreply.github.com>
2023-05-03 15:24:43 +0000

    Wed, May 3, 2023, 3:24 PM +00:00

--- Psets/pset1/extentions.py
+++ Psets/pset1/extentions.py
@@ -34,3 +34,7 @@ else:
 # however, i feel like there could be a better way to design it
 # but on the other hand, each if statement has a different outcome, so i think this is pretty close to the best design or my level of knowledge is not good enough at this point.
 # What if there was 120 different file extentions ?  we can't code them one by one like this
+
+#But in the str documentation, they say if you are checking to see if a sub is in a str, you should use "in" keyword instead of str.find() function.
+
+#str.find() function finds the locaion  of a substring in a str
