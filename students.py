4d9e1bd9215be3dafa12cbf1e4023f0dc7ba1f23
mTheDingo <114156590+mTheDingo@users.noreply.github.com>
2023-05-08 18:08:59 +0000

    Mon, May 8, 2023, 6:08 PM +00:00

--- students.py
+++ students.py
@@ -1,9 +1,4 @@
+name = input("What's your name? ")
 
-names = []
-
-for _ in range(3):
-    names.append(input("What's your name ? "))  #.append -() appends values to the list
-
-
-for name in sorted(names):  #sorted() sorts the list for us.
-    print(f"hello, {name}")
+with open("names.txt", "a") as file:
+     file.write(f"{name}\n")
